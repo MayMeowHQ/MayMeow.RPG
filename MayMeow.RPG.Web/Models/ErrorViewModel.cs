57da6cef5f1b7b1ce2c616711e93e6817af4ee8d
Emma Watson <themaymeow@gmail.com>
2020-09-01 20:36:09 +0200

    Add project files.

---
+++ MayMeow.RPG.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MayMeow.RPG.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

8d7bca87396c7a998296e780c6fb51addead5564
Emma Watson <themaymeow@gmail.com>
2020-09-01 21:39:05 +0200

    Add Admin area route

--- MayMeow.RPG.Data/Startup.cs
+++ MayMeow.RPG.Data/Startup.cs
@@ -30,6 +30,12 @@ namespace MayMeow.RPG.Data
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapAreaControllerRoute(
+                    "Admin_route",
+                    areaName: "Admin",
+                    pattern: "-/Admin/{controller=Home}/{action=Index}/{id?}"
+                );
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");

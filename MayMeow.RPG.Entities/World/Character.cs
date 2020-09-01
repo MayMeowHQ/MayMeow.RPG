7e25844153fce727e1d85000345779f09e10675e
Emma Watson <themaymeow@gmail.com>
2020-09-01 21:27:29 +0200

    Add World Models

---
+++ MayMeow.RPG.Entities/World/Character.cs
@@ -0,0 +1,51 @@
+﻿using MayMeow.RPG.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MayMeow.RPG.Entities.World
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+
+        //
+        public int Experience { get; set; }
+        public int Money { get; set; }
+
+        // Attributes
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+
+        // Stats
+        public int HitPoints { get; set; }
+        public int TotalHitPoints { get; set; }
+
+        // Information About Owner
+        public string OwnerId { get; set; }
+        [ForeignKey("OwnerId")]
+        public ApplicationUser Owner { get; set; }
+
+        // Time informations
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Race information
+        public int RaceId { get; set; }
+        [ForeignKey("RaceId")]
+        public Race Race { get; set; }
+    }
+
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+}
---
+++ MayMeow.RPG.Entities/World/Race.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MayMeow.RPG.Entities.World
+{
+    public class Race
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        // Attributes
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Charisma { get; set; }
+
+        // Home location information
+        public int StartingLocationId { get; set; }
+        [ForeignKey("StartingLocationId")]
+        public Location StartingLocation { get; set; }
+    }
+}
